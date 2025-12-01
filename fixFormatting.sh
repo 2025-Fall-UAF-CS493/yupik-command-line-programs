7b191d45937eae9eee624c6d5924263c74ff1c11
Ian Rodriguez <148839368+CloudEerie@users.noreply.github.com>
2025-11-30 21:01:25 -0900

    Fixed outdated CSYReplacements entries.

--- fixFormatting.sh
+++ fixFormatting.sh
@@ -6,7 +6,7 @@ abbrvNL=("PI" "SPI" "NAI" "WCI" "GRI" "ECI" "PI" "CSY" "AAY" "CAY" "NSY" "NWI" "
 fontSymbols=("p" "t" "k" "kw" "q" "qw" "v" "l" "z" "y" "$" "!" "!w" "3" "3w" "f" "@" "s" "5" "x" "xw" "X" "Xw" "h" "m" "n" "&" "&w" "6" "%" "7" "7w" "i" "u" "0" "a" "4" "9" "\\")
 
 CAYReplacements=("p" "t" "k" "" "q" "" "v" "l" "s" "y" "" "g" "ug" "r" "" "vv" "ll" "ss" "" "gg" "w" "rr" "" "h" "m" "n" "ng" "" "ḿ" "ń" "ńg" "" "i" "u" "e" "a" "" "" "")
-CSYReplacements=("p" "t" "k" "kw" "q" "qw" "v" "l" "z" "y" "r" "g" "w" "gh" "ghw" "f" "ll" "s" "rr" "gg" "w" "gh" "ghw" "h" "m" "n" "ng" "ngw" "ḿ" "ń" "ńg" "ńgw" "i" "u" "e" "a" "" "" "")
+CSYReplacements=("p" "t" "k" "kw" "q" "qw" "v" "l" "z" "y" "r" "g" "w" "gh" "ghw" "f" "ll" "s" "rr" "gg" "wh" "ghh" "ghhw" "h" "m" "n" "ng" "ngw" "mm" "nn" "ngng" "ngngw" "i" "u" "e" "a" "" "" "")
 APAReplacements=("p" "t" "k" "kʷ" "q" "qʷ" "v" "l" "z" "y" "ž" "ɣ" "ɣʷ" "ʀ" "ʀʷ" "f" "ł" "s" "š" "x" "xʷ" "X" "Xʷ" "h" "m" "n" "ŋ" "ŋʷ" "m̥" "n̥" "ŋ̥" "ŋ̥ʷ" "i" "u" "ə" "a" "ɨ" "ð" "R̃")
 IPAReplacements=($'p' $'t' $'k' $'k\u02B7' $'q' $'q\u02B7' $'v' $'l' $'z' $'j' $'\u0290' $'\u0263' $'\u0263\u02B7' $'\u0281' $'\u0281\u02B7' $'f' $'\u026C' $'s' $'\u0282' $'x' $'x\u02B7' $'\u03C7' $'X\u02B7' $'h' $'m' $'n' $'\u014B' $'\u014B\u02B7' $'m\u0325' $'n\u0325' $'\u014B\u0325' $'\u014B\u0325\u02B7' $'i' $'u' $'\u0259' $'a' $'\u0268' $'\u00F0' $'\u0274')
 # Index 7: The APA->IPA file from ling. group has 'l' with theta as its unicode. Leaving this here until I can clarify
@@ -227,8 +227,8 @@ rm "$tempFile"
 
 sed -i -E "s/([0-9]+)([A-Z]{2,3}\b)/\1\n\2/g" "$outputFile"
 for langTag in "${abbrvNL[@]}"; do
-	sed -i -E "s/\b($langTag)\b/\n\1/g" "$outputFile"; # Newline after each langTang
-done 
+	sed -i -E "s/\b($langTag)\b/\n\1/g" "$outputFile"; # Newline after each langTag
+done
 
 awk 'BEGIN { FS=" "; OFS="\t"; currentPage="" }
 {
